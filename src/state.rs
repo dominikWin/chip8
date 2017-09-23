decceb48185eb7576b64b1e2a3da3f1badfca1be
Dominik Winecki <dominikwinecki@gmail.com>
2017-09-23 14:09:45 -0400

    Add eq tests for state

--- src/state.rs
+++ src/state.rs
@@ -3,13 +3,13 @@ use std::fmt;
 use std::cmp;
 
 pub struct Chip8State {
-    vregs: [u8; 16],
-    i: u16,
-    sp: u16,
-    pc: u16,
-    delay: u8,
-    sound: u8,
-    mem: [u8; 0x1000],
+    pub vregs: [u8; 16],
+    pub i: u16,
+    pub sp: u16,
+    pub pc: u16,
+    pub delay: u8,
+    pub sound: u8,
+    pub mem: [u8; 0x1000],
 }
 
 impl Chip8State {
@@ -37,18 +37,23 @@ impl Chip8State {
 
 impl fmt::Debug for Chip8State {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Chip8State {{ vregs: {:?}, i: {:?}, sp: {:?}, pc: {:?}, delay: {:?}, sound: {:?} }}", self.vregs, self.i, self.sp, self.pc, self.delay, self.sound)
+        write!(
+            f,
+            "Chip8State {{ vregs: {:?}, i: {:?}, sp: {:?}, pc: {:?}, delay: {:?}, sound: {:?} }}",
+            self.vregs,
+            self.i,
+            self.sp,
+            self.pc,
+            self.delay,
+            self.sound
+        )
     }
 }
 
 impl cmp::PartialEq for Chip8State {
     fn eq(&self, other: &Chip8State) -> bool {
-        self.vregs == other.vregs &&
-            self.i == other.i &&
-            self.sp == other.sp &&
-            self.pc == other.pc &&
-            self.delay == other.delay &&
-            self.sound == other.sound &&
+        self.vregs == other.vregs && self.i == other.i && self.sp == other.sp &&
+            self.pc == other.pc && self.delay == other.delay && self.sound == other.sound &&
             {
                 let mut eq = true;
                 for i in 0..self.mem.len() {
@@ -71,5 +76,52 @@ mod tests {
         let mut tmp = Chip8State::new();
         tmp.load_program(&Chip8Program::new(&[0u8; 0]));
         assert_eq!(Chip8State::new(), tmp);
+    }
+
+    #[test]
+    fn test_eq() {
+        assert_eq!(Chip8State::new(), Chip8State::new());
+
+        let mut tmp = Chip8State::new();
+        tmp.load_program(&Chip8Program::new(&[0u8; 0]));
+        assert_eq!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.load_program(&Chip8Program::new(&[0x00, 0x01]));
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp1 = Chip8State::new();
+        let mut tmp2 = Chip8State::new();
+        tmp1.load_program(&Chip8Program::new(&[0x00, 0x01]));
+        tmp2.load_program(&Chip8Program::new(&[0x00, 0x01]));
+        assert_eq!(tmp1, tmp2);
+
+        let mut tmp = Chip8State::new();
+        tmp.vregs[3] = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.vregs[3] = 0x00;
+        assert_eq!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.i = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.sp = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.pc = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.delay = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
+
+        let mut tmp = Chip8State::new();
+        tmp.sound = 0x34;
+        assert_ne!(Chip8State::new(), tmp);
     }
 }
