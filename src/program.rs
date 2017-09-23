de95f66060539a876be7ae607e18f22af78d1c7a
Dominik Winecki <dominikwinecki@gmail.com>
2017-09-23 14:12:17 -0400

    Format Chip8Program

--- src/program.rs
+++ src/program.rs
@@ -14,12 +14,10 @@ impl Chip8Program {
             warn!("Creating program ending with half an instruction");
         }
         for i in 0..source.len() / 2 {
-            let instruction: u16 = ((source[i * 2] as u16) << 8) | (source[i*2+1] as u16);
+            let instruction: u16 = ((source[i * 2] as u16) << 8) | (source[i * 2 + 1] as u16);
             instructions.push(instruction);
         }
-        Chip8Program {
-            instructions,
-        }
+        Chip8Program { instructions }
     }
 
     pub fn from(mut source: Box<io::Read>) -> Result<Chip8Program, io::Error> {
@@ -39,13 +37,37 @@ mod tests {
 
     #[test]
     fn test_new() {
-        assert_eq!(Chip8Program{instructions: vec![]}, Chip8Program::new(&[]));
-        assert_eq!(Chip8Program{instructions: vec![]}, Chip8Program::new(&[0x12]));
-        assert_eq!(Chip8Program{instructions: vec![0x0000]}, Chip8Program::new(&[0x00, 0x00]));
-        assert_eq!(Chip8Program{instructions: vec![0x1234]}, Chip8Program::new(&[0x12, 0x34]));
-        assert_eq!(Chip8Program{instructions: vec![0x1234]}, Chip8Program::new(&[0x12, 0x34, 0x12]));
-        assert_eq!(Chip8Program{instructions: vec![0x1234, 0x1242]}, Chip8Program::new(&[0x12, 0x34, 0x12, 0x42]));
-        assert_eq!(Chip8Program{instructions: vec![0x1234, 0x1242, 0xabcd]}, Chip8Program::new(&[0x12, 0x34, 0x12, 0x42, 0xab, 0xcd]));
-        assert_eq!(Chip8Program{instructions: vec![0x1234, 0x1242, 0xabcd]}, Chip8Program::new(&[0x12, 0x34, 0x12, 0x42, 0xab, 0xcd, 0xff]));
+        assert_eq!(
+            Chip8Program { instructions: vec![] },
+            Chip8Program::new(&[])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![] },
+            Chip8Program::new(&[0x12])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x0000] },
+            Chip8Program::new(&[0x00, 0x00])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x1234] },
+            Chip8Program::new(&[0x12, 0x34])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x1234] },
+            Chip8Program::new(&[0x12, 0x34, 0x12])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x1234, 0x1242] },
+            Chip8Program::new(&[0x12, 0x34, 0x12, 0x42])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x1234, 0x1242, 0xabcd] },
+            Chip8Program::new(&[0x12, 0x34, 0x12, 0x42, 0xab, 0xcd])
+        );
+        assert_eq!(
+            Chip8Program { instructions: vec![0x1234, 0x1242, 0xabcd] },
+            Chip8Program::new(&[0x12, 0x34, 0x12, 0x42, 0xab, 0xcd, 0xff])
+        );
     }
 }
